92acaca37c8d0c906b4348fe8aeab98a7fec2eda
Haidar Diab <dyab332@gmail.com>
2016-05-12 23:45:14 +0300

    Initial Commit

---
+++ LuGradesBot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+using OpenQA.Selenium;
+
+
+
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Chrome;
+
+namespace LuGradesBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//using(IWebDriver driver = new ChromeDriver(@"C:\Program Files (x86)\chromedriver_win32"))
+			//{
+			//	driver.Navigate().GoToUrl("http://ulfg.ul.edu.lb/login.aspx");
+			//	IWebElement element = driver.FindElement(By.Id("form1"));
+			//	Console.WriteLine(element.TagName);
+			//	Console.ReadKey();
+
+			}
+		}		
+	}
+}
